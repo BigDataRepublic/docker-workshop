ea2ca30e66823e66632786bdcbe661cc9be0a2d9
Emiel de Heij <emieldeheij@hotmail.com>
2022-05-17 00:00:26 +0200

    apply black and change github account

--- main.py
+++ main.py
@@ -5,18 +5,21 @@ import pandas as pd
 
 app = FastAPI()
 
-lgbm_model = joblib.load('model/lgbm_model.joblib')
-enc = joblib.load('model/encoder.joblib')
+lgbm_model = joblib.load("model/lgbm_model.joblib")
+enc = joblib.load("model/encoder.joblib")
+
 
 @app.get("/")
 def welcome_msg():
-    return{"message":"Hello World!"}
+    return {"message": "Hello World!"}
+
 
 @app.post("/predict")
 def return_prediction(query: str):
     query = pd.read_json(query)
     query = enc.transform(query)
-    return{"message":"Success", "Prediction":lgbm_model.predict(query)}
+    return {"message": "Success", "Prediction": lgbm_model.predict(query)}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8080)
